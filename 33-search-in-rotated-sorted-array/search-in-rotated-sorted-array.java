938d1b005f4a40201912bb99c7accafa52bc3f78
SaikumarMysa <164009328+SaikumarMysa@users.noreply.github.com>
2026-07-04 06:41:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (81.68%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -3,9 +3,34 @@ class Solution {
         int pivot = 0;
         int n = nums.length;
 
-        for(int i=0;i<n;i++){
-            if(nums[i]==target){
-                return i;
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1]){
+                continue;
+            }else{
+                pivot=i;
+                break;
+            }
+        }
+        int l=0, h=pivot-1;
+        while(l<=h){
+            int m=l+(h-l)/2;
+            if(nums[m]==target){
+                return m;
+            }else if(nums[m]>target){
+                h=m-1;
+            }else{
+                l=m+1;
+            }
+        }
+        int low=pivot, high=n-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==target){
+                return mid;
+            }else if(nums[mid]>target){
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
            
