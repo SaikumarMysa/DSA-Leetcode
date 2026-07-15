97f378d6c6b4d1ce777c7ae974db491bb52c7584
SaikumarMysa <164009328+SaikumarMysa@users.noreply.github.com>
2026-07-15 19:58:57 +0530

    Time: 50 ms (5.42%) | Memory: 88.7 MB (78.31%) - LeetSync

--- 164-maximum-gap/maximum-gap.java
+++ 164-maximum-gap/maximum-gap.java
@@ -1,10 +1,49 @@
 class Solution {
+    public static int partition(int[] arr, int low, int high){
+        int pivot = arr[low];
+
+        int i = low-1;
+
+        int j= high+1;
+
+        while(true){
+            do{
+                i++;
+            }while(arr[i]<pivot);
+
+            do{
+                j--;
+            }while(arr[j]>pivot);
+
+            if(i>=j){
+                return j;
+            }
+
+            swap(arr, i, j);
+        }
+    }
+
+    public static void swap(int arr[], int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    public static void quickSort(int nums[], int low, int high){
+        if(low<high){
+            int p = partition(nums, low, high);
+            quickSort(nums, low, p);
+            quickSort(nums, p+1, high);
+        }
+    }
     public int maximumGap(int[] nums) {
         int n = nums.length;
 
         if(n<2) return 0;
 
-        Arrays.sort(nums); // 1 3 6 9 
+        int high = n-1;
+
+        quickSort(nums, 0, high); // 1 3 6 9 
 
         int max = -1;
         for(int i=1; i<n; i++){
