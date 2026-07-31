5c6aa1f1a889c41e77f3c275e633a6236b2c32e1
SaikumarMysa <164009328+SaikumarMysa@users.noreply.github.com>
2026-07-31 23:27:01 +0530

    Time: 3 ms (8.89%) | Memory: 43.3 MB (18.71%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,19 +1,49 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
-       int m=needle.length();
-       int n= haystack.length();
-
-       for(int i=0; i<=n-m; i++){
-        int j;
-        for(j=0;j<m;j++){
-            if(haystack.charAt(i+j)!=needle.charAt(j)){
-                break;
+    //Build the LPS
+    public static int[] buildLPS(String pattern){
+        int m=pattern.length();
+        int[] lps = new int[m];
+        int len=0;
+        int i=1;
+        while(i<m){
+            if(pattern.charAt(i)==pattern.charAt(len)){
+                len++;
+                lps[i]=len;
+                i++;
+            }else{
+                if(len!=0){
+                    len=lps[len-1];
+                }else{
+                    lps[i]=0;
+                    i++;
+                }
             }
         }
-        if(j==m){
-            return i;
+        return lps;
+    }
+
+    public static int strStr(String haystack, String needle) {
+        int ans=-1;
+        int n=haystack.length();
+        int m=needle.length();
+        int[]lps=buildLPS(needle);
+        int i=0,j=0;
+        while(i<n){
+            if(haystack.charAt(i)==needle.charAt(j)){
+                i++;
+                j++;
+            }
+            if(j==m){
+                return (i-j);
+                //j=lps[j-1];
+            }else if(i<n&&haystack.charAt(i)!=needle.charAt(j)){
+                if(j!=0){
+                    j=lps[j-1];
+                }else{
+                    i++;
+                }
+            }
         }
-       }
-       return -1;
+        return ans;
     }
 }
