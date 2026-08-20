a16f4de1db87d808292003458b27f356e7ea6156
SaikumarMysa <164009328+SaikumarMysa@users.noreply.github.com>
2026-08-20 12:48:24 +0530

    Time: 3 ms (46.63%) | Memory: 45.3 MB (12.64%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,45 +1,27 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        int n1 = nums1.length;
-        int n2 = nums2.length;
+        HashSet<Integer> hs1 = new HashSet<>();
+        HashSet<Integer> hs2 = new HashSet<>();
 
-        ArrayList<Integer> list = new ArrayList<>();
+        for(int i=0; i<nums1.length; i++){
+            hs1.add(nums1[i]);
+        }
+        for(int i=0; i<nums2.length; i++){
+            hs2.add(nums2[i]);
+        }
 
-        HashMap<Integer, Integer> hm1 = new HashMap<>(); 
-        HashMap<Integer, Integer> hm2 = new HashMap<>();
-
-        for(int i=0;i<n1;i++){
-            int key = nums1[i]; 
-            if(hm1.containsKey(key)){
-                int currVal = hm1.get(key);
-                hm1.put(key, currVal+1);
-            }else{
-                hm1.put(key, 1);
+        ArrayList<Integer> al = new ArrayList<>();
+        for(int element:hs1){
+            if(hs2.contains(element)){
+                al.add(element);
             }
         }
 
-        for(int i=0;i<n2;i++){
-            int key = nums2[i]; 
-            if(hm2.containsKey(key)){
-                int curr = hm2.get(key);
-                hm2.put(key, curr+1);
-            }else{
-                hm2.put(key, 1);
-            }
-        }
+        int arr[] = new int[al.size()];
 
-        for(int val: hm1.keySet()){
-            if(hm2.containsKey(val)){
-                list.add(val);
-            }
+        for(int i=0; i<al.size(); i++){
+            arr[i] =  al.get(i);
         }
-
-        int [] arr = new int [list.size()];
-
-        for(int i=0;i<arr.length;i++){
-            arr[i] = list.get(i);
-        }
-       return arr; 
+    return arr;
     }
-    
 }
